8b177fa03a90811a278004531fb449a403d9a29a
Ming-der Wang <mingder78@yahoo.com>
2020-11-22 06:30:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+student_grades = [57, 74, 49, 0, 87, 66, 89]
+print(type(student_grades))
+print('-------- use for loop on a list')
+for g in student_grades:
+  print(g)
+  if g >= 52: 
+    print('This student passed.')
+  else:
+    print('This student failed.')
+
+print('Program complete')
+print()
+print('-------- use iter in a while loop ---')
+sg = iter(student_grades)
+print(type(sg))
+while True:
+  try:
+    g = next(sg)
+    print(g)
+    if g >= 52: 
+      print('This student passed.')
+    else:
+      print('This student failed.')
+  except StopIteration:
+    print('Program complete')
+    break
